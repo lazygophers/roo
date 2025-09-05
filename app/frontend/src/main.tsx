a8fef7c04ea7224a138d013eba598f0c5c0f9014
nico <nico@lofreer.com>
2025-09-05 19:54:17 +0800

    feat(frontend): 添加 React + TypeScript + Vite 项目模板
    - 新建前端项目目录和文件结构
    - 添加 Vite 配置和 React 组件
    - 配置 TypeScript 和 ESLint
    - 添加 Ant Design 组件库
    - 新建 Git 忽略文件

---
+++ app/frontend/src/main.tsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { ConfigProvider, theme } from 'antd';
+import './index.css'
+import 'antd/dist/reset.css';
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ConfigProvider theme={{
+      algorithm: theme.darkAlgorithm,
+    }}>
+      <App />
+    </ConfigProvider>
+  </StrictMode>,
+)
